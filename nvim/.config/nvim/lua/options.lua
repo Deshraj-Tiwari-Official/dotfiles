b0eab5abeb1b03defffc80262d276ac893f2a194
Deshraj Tiwari <deshrajtiwari690@gmail.com>
2024-06-26 14:21:04 +0530

    Rofi config and NVIM change

--- nvim/.config/nvim/lua/options.lua
+++ nvim/.config/nvim/lua/options.lua
@@ -1,5 +1,5 @@
 require "nvchad.options"
 local o = vim.o
-o.cursorlineopt ='both' -- to enable cursorline!
+o.cursorlineopt = 'both' -- to enable cursorline!
 o.clipboard = 'unnamedplus' -- to enable system clipboard!
 o.relativenumber = true -- to enable relative numbering!
