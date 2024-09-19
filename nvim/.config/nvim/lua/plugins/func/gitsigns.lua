3ce4184ddfa04ecc87677cc9396798ea8f7326bb
Deshraj-Tiwari-Official <deshrajtiwari690@gmail.com>
2024-09-20 00:14:48 +0530

    Even minimal Gitsigns

--- nvim/.config/nvim/lua/plugins/func/gitsigns.lua
+++ nvim/.config/nvim/lua/plugins/func/gitsigns.lua
@@ -1,55 +1,11 @@
 return {
-    "lewis6991/gitsigns.nvim",
-    config = function()
-        require('gitsigns').setup {
-            on_attach = function(bufnr)
-                local gitsigns = require('gitsigns')
-
-                local function map(mode, l, r, opts)
-                    opts = opts or {}
-                    opts.buffer = bufnr
-                    vim.keymap.set(mode, l, r, opts)
-                end
-
-                -- Navigation
-                map('n', ']c', function()
-                    if vim.wo.diff then
-                        vim.cmd.normal({ ']c', bang = true })
-                    else
-                        gitsigns.nav_hunk('next')
-                    end
-                end)
-
-                map('n', '[c', function()
-                    if vim.wo.diff then
-                        vim.cmd.normal({ '[c', bang = true })
-                    else
-                        gitsigns.nav_hunk('prev')
-                    end
-                end)
-
-                -- Actions
-                map('n', '<leader>hp', gitsigns.preview_hunk)
-                map('n', '<leader>tb', gitsigns.toggle_current_line_blame)
-
-                -- There are some other bindings that you might like to setup
-                --
-                -- map('n', '<leader>hs', gitsigns.stage_hunk)
-                -- map('n', '<leader>hr', gitsigns.reset_hunk)
-                -- map('v', '<leader>hs', function() gitsigns.stage_hunk { vim.fn.line('.'), vim.fn.line('v') } end)
-                -- map('v', '<leader>hr', function() gitsigns.reset_hunk { vim.fn.line('.'), vim.fn.line('v') } end)
-                -- map('n', '<leader>hS', gitsigns.stage_buffer)
-                -- map('n', '<leader>hu', gitsigns.undo_stage_hunk)
-                -- map('n', '<leader>hR', gitsigns.reset_buffer)
-                -- map('n', '<leader>hb', function() gitsigns.blame_line { full = true } end)
-                -- map('n', '<leader>hd', gitsigns.diffthis)
-                -- map('n', '<leader>hD', function() gitsigns.diffthis('~') end)
-                -- map('n', '<leader>td', gitsigns.toggle_deleted)
-
-                -- Text object
-                map({ 'o', 'x' }, 'ih', ':<C-U>Gitsigns select_hunk<CR>')
-            end
-        }
-    end
+	"lewis6991/gitsigns.nvim",
+	config = function()
+		require("gitsigns").setup({
+			-- Actions
+			vim.keymap.set("n", "<leader>hp", require("gitsigns").preview_hunk),
+			vim.keymap.set("n", "<leader>tb", require("gitsigns").toggle_current_line_blame),
+			-- There are some other bindings that you might like to setup
+		})
+	end,
 }
-
