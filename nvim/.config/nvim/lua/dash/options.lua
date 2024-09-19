14fb80e8d61802c5fbf6283b8e287f74f1caf83b
Deshraj-Tiwari-Official <deshrajtiwari690@gmail.com>
2024-09-19 22:52:01 +0530

    Highlight on yank full autocommand setup
    Highlight on yank was just added as a property without an auto command,
    IDK why. So I fixed it :)

--- nvim/.config/nvim/lua/dash/options.lua
+++ nvim/.config/nvim/lua/dash/options.lua
@@ -1,4 +1,11 @@
-vim.highlight.on_yank()
+vim.api.nvim_create_autocmd("TextYankPost", {
+  desc = "Highlight when yanking (copying) text",
+  group = vim.api.nvim_create_augroup("kickstart-highlight-yank", { clear = true }),
+  callback = function()
+    vim.highlight.on_yank()
+  end,
+})
+
 vim.g.autoformat = true
 
 local opt = vim.opt
