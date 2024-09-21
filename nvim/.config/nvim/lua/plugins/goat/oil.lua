0827554f6c151e825e217f1d9be0a3bdbfb53759
Deshraj-Tiwari-Official <deshrajtiwari690@gmail.com>
2024-09-21 14:01:07 +0530

    Oil hiding .github

--- nvim/.config/nvim/lua/plugins/goat/oil.lua
+++ nvim/.config/nvim/lua/plugins/goat/oil.lua
@@ -11,7 +11,7 @@ return {
             view_options = {
                 show_hidden = true,
                 is_always_hidden = function (name, _)
-                    return name == ".git" or name == ".."
+                    return name == ".git" or name == ".." or name == ".github"
                 end,
             }
 		})
