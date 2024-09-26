6ac9f3071828ef69212589ef445c134ad8ace825
Deshraj-Tiwari-Official <deshrajtiwari690@gmail.com>
2024-09-26 16:12:45 +0530

    Re-adding comment.nvim

---
+++ nvim/.config/nvim/lua/dash/plugins/comments.lua
@@ -0,0 +1,26 @@
+return {
+	"numToStr/Comment.nvim",
+	opts = {
+		{
+			padding = true,
+			sticky = true,
+			toggler = {
+				line = "gcc",
+				block = "gbc",
+			},
+			opleader = {
+				line = "gc",
+				block = "gb",
+			},
+			extra = {
+				above = "gca",
+				below = "gcb",
+				eol = "gce",
+			},
+			mappings = {
+				basic = true,
+				extra = true,
+			},
+		},
+	},
+}
